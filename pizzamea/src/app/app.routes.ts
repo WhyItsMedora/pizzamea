46aadb9bcd5733c63e54f462b2967c7e35bdb1ea
WhyItsMedora <medorasailer@gmail.com>
2026-05-07 16:19:11 -0500

    Fixed buttons

--- pizzamea/src/app/app.routes.ts
+++ pizzamea/src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Routes } from '@angular/router';
 import { Homepage } from './homepage/homepage';
 import { Cart } from './cart/cart';
 import { Confirmation } from './confirmation/confirmation';
-
 import { Menu } from './menu/menu';
 import { CustomizePizza } from './customize-pizza/customize-pizza';
-// import { Contact } from './contact/contact';
-// import { Cart } from './cart/cart';
 
 export const routes: Routes = [
-    {path: '', component: Homepage},
-    
+  { path: '', component: Homepage },
   { path: 'menu', component: Menu },
   { path: 'customize-pizza', component: CustomizePizza },
   { path: 'cart', component: Cart },
